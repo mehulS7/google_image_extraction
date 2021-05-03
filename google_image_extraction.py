c404530a74ee2ab7007bab3fb43c899dd7713213
Mehul Sachdeva <Mehul.Sachdeva7@gmail.com>
2021-05-03 18:56:43 +0530

    approach includes tab rotation

--- google_image_extraction.py
+++ google_image_extraction.py
@@ -3,53 +3,88 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from bs4 import BeautifulSoup as soup
 from urllib import request as req
 import re
+from pathlib import Path
+import time
+import requests
 
-print('Please install Chrome and Chrome driver befrore running the script. Thanks')
+print('Please install Chrome and Chrome Web driver befrore running the script. Thanks')
 
-path = input("Please enter the driver path.\nExample: C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe\n>").strip()
-#C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe
+path = Path(input("Please enter the driver path.\n>").strip())
+# Windows -> C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe
+# MAC -> X:/Users/username/Desktop
 
-downloads = input("Please enter the path where you would want to store the images.\nExample: C:\\Users\\yourname\\Images\n>").strip()
-#'C:\Users\yourname\images'
+downloads = input("Please enter the path where you would want to store the images.\n>").strip()
 
 keyword = input('Enter the keyword:\n>')
 
 def saveimg(mark,link,fn):
-    page = req.urlopen(link)
-    cont = page.read()
-    page.close()
+    try:
+        response = requests.get(link)
+        cont = response.content
+    except:
+        page = req.urlopen(link)
+        cont = page.read()
+        page.close()
     fn = str(mark) + '_' + re.split('[/\\:\|?]',fn)[0].strip()
     print('Title: ' + fn + ' ... Downloaded')
-    f = open(downloads + '\\' + fn + '.png','wb')
+    f = open(Path(downloads + '\\' + fn + '.png'),'wb')
     f.write(cont)
     f.close()
     
 
+# def movetab(mark,alt):
+#     time.sleep(1)
+#     driver.switch_to.window(driver.window_handles[1])
+#     time.sleep(4)
+#     page2_soup = soup(driver.page_source,'html.parser')
+#     link = page2_soup.find('img',{'alt' : alt})['src']
+#     saveimg(mark,link,alt)
+#     driver.close()
+
 driver = webdriver.Chrome(path)
-driver.get('http://google.com/images')
-driver.minimize_window()
-search =  WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH,'/html/body/div[2]/div[2]/div[2]/form/div[1]/div[1]/div[1]/div/div[2]/input')))
+driver.get('https://www.google.com/imghp')
+search =  WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH,'/html/body/div[2]/div[2]/div[2]/form/div[1]/div[1]/div[1]/div/div[2]/input')))
 search.send_keys(keyword)
 search.send_keys(Keys.RETURN)
-driver.implicitly_wait(5)
-page_soup = soup(driver.page_source,'html.parser')
-imgs = page_soup.findAll('img',{'class' : 'rg_i Q4LuWd'})
-print(f'Maximum number of images that can be downloaded are: {len(imgs)}')
+# time.sleep(2)
+# page_soup = soup(driver.page_source,'html.parser')
+# imgs = page_soup.findAll('img',{'class' : 'rg_i Q4LuWd'})
 
-if len(imgs) !=0:
-    imgReq = int(input('Number of images need to be downloaded:\n>'))
+# print(f'Maximum number of images that can be downloaded are: {len(imgs)}')
 
-for n,im in enumerate(imgs):
-    if n+1 > imgReq:
-        break
-    else:
-        print('Image no: ',n+1)
-        if 'src' in im.attrs.keys():
-            saveimg(n+1,im['src'],im['alt'])
-        else:
-            saveimg(n+1,im['data-src'],im['alt'])
+# if len(imgs) !=0:
+    
+imgReq = int(input('Number of images need to be downloaded:\n>'))
+
+i = 0
+inc = 1
+limit = 4
+
+while(i<imgReq):
+    try:
+        thumb = driver.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[1]/div[1]/div[' + str(inc) + ']/a[1]')
+    except:
+        if limit == 0:
+            print("Can't find next Element. Limit set to 5 jumps.")
+            break
+        limit -= 1
+        inc += 1
+        continue
+    print('Image: ',i+1)
+    thumb.click()
+    time.sleep(5)
+    bigImg = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH,'/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div/div[2]/a/img')))
+    alt = bigImg.get_attribute('alt')
+    link = bigImg.get_attribute('src')
+    print('alt:',alt,'\nlink: ',link)
+    saveimg(i+1, link, alt)
+    # thumb.send_keys(Keys.CONTROL + Keys.RETURN)
+    # movetab(i+1,alt)
+    # driver.switch_to.window(driver.window_handles[0])
+    inc += 1
+    i += 1
+    limit = 5
 
 driver.quit()
